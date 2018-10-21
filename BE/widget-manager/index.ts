1dc9645120308c27c20ab64f3f1eed88ff2f4931
sharstyuk_a <sharstyuk_a@lwo.by>
2018-10-21 21:09:31 +0300

    remake index.ts

--- BE/widget-manager/index.ts
+++ BE/widget-manager/index.ts
@@ -2,6 +2,12 @@ import express = require('express');
 import bodyParser = require("body-parser");
 import cors = require("cors");
 import {Widgets} from "./models";
+import passport = require('passport');
+
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GOOGLE_CLIENT_ID = '815735416365-a3vvu0e4avquai6sranjqbu9pbpqegur.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '7fOru3skiJ3DVrdS1chErlUn';
+const app = express();
 
 const corsOptions = {
     origin: ["http://localhost:4200", "https://www.googleapis.com/auth/plus.login"],
@@ -9,11 +15,6 @@ const corsOptions = {
     preflightContinue: false,
     optionsSuccessStatus: 204
 };
-
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors(corsOptions));
 
 /**
  * Имитация базы
@@ -44,70 +45,57 @@ let usersWidgets: any = {
     }]
 };
 
-/**
- * получение виджетов для меню
- * */
-app.get('/widgets', function (req, res) {
-    res.send(widgets);
+passport.use(new GoogleStrategy({clientID: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET, callbackURL: "http://localhost:4200"}, (accessToken, refreshToken, profile, done) => {
+    console.log('accessToken', accessToken, 'refreshToken', refreshToken, 'profile', profile, 'done',  done)
+}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+
+app.all('/*', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
 });
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login']}));
 
 /**
  * получение виджетов для определенного юзера
  * */
-app.get('/widgets/user', function (req, res) {
+app.get('/widgets/user', (req, res) => {
     res.send(usersWidgets);
 });
 
 /**
  * обновление виджета
  * */
-app.put('/put', function (req, res) {
+app.put('/put', (req, res) => {
     res.send('work put')
 });
 
 /**
  * создание виджета
  * */
-app.delete('/delete-widgets', function (req, res) {
+app.delete('/delete-widgets', (req, res) => {
     res.send('work post')
 });
 
 /**
  * удаление виджета
  * */
-app.delete('/delete', function (req, res) {
+app.delete('/delete', (req, res) => {
     res.send('work delete')
+});
+
+/**
+ * получение виджетов для меню
+ * */
+app.get('/widgets', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
+    res.send(widgets);
+    res.redirect('/');
 });
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
-});
-
-import passport = require('passport');
-
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const GOOGLE_CLIENT_ID = '815735416365-a3vvu0e4avquai6sranjqbu9pbpqegur.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = '7fOru3skiJ3DVrdS1chErlUn';
-
-passport.use(new GoogleStrategy({
-        clientID: GOOGLE_CLIENT_ID,
-        clientSecret: GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:4200/widgets"
-    },
-    (accessToken, refreshToken, profile, done) => {
-        console.log('accessToken', accessToken, 'refreshToken', refreshToken, 'profile', profile, 'done',  done)
-    }
-));
-
-app.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login']}));
-
-app.get('/widgets', passport.authenticate('google', { failureRedirect: '/login' }),
-    function(req, res) {
-    debugger;
-        res.redirect('/');
-    });
-
-app.all('/*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
 });
